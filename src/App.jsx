fd109b6773ce77bc21f1e901df4f888e8800177f
Luis Cisneros <tiko.cisneros@gmail.com>
2023-10-22 21:15:12 -0500

    feat: fetch and list tasks from server

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from '@chakra-ui/react'
+import useAxios from 'axios-hooks';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [{ data, loading, error }] = useAxios('http://localhost:8100/tasks');
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Button colorScheme='cyan'>Button</Button>
-    </>
-  )
+    <ul>
+      {data.map(({ id, name }) => (
+        <li key={id}>{name}</li>
+      ))}
+    </ul>
+  );
 }
 
-export default App
+export default App;
